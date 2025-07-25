9cc63077b1d00e5b4dfe1e01c37cf865c2622a90
loopset <miguellozanogonzalez119@gmail.com>
2025-07-24 20:10:24 -0700

    bugfix rsync script

--- doRsync.sh
+++ doRsync.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Identity file 
+SSH_KEY="$1"
+
 # From where
 remote="rcmp@142.90.96.93"
 
@@ -9,5 +12,5 @@ dirs=("Raw" "Cluster" "Data" "Filter" "Merger")
 #Act on dirs
 for d in "${dirs[@]}"; do
   echo "Rsyncing ${d}"
-  rsync -e "ssh -i ~/.ssh/rcmp_at_triumf" -avz "${remote}:/home/rcmp/S2384/RootFiles/${d}/" "./RootFiles/{d}/"
+  rsync -e "ssh -i ${SSH_KEY}" -avz "${remote}:/home/rcmp/S2384/RootFiles/${d}/" "./RootFiles/{d}/"
 done
