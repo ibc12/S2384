aaba8d41aee7dc06d2d9f939b8e950b93ec41dba
loopset <miguellozanogonzalez119@gmail.com>
2025-07-24 20:17:33 -0700

    bugfix to dorsync

--- doRsync.sh
+++ doRsync.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# Identity file 
-SSH_KEY="$1"
+# Identity file
+if [ "$1" == "ibc" ]; then
+  SSH_KEY="$HOME/.ssh/rcmp_at_triumf_ibc"
+elif [ "$1" == "mlg" ]; then
+  SSH_KEY="$HOME/.ssh/rcmp_at_triumf"
+else
+  echo "Error: first argument must be either ibc or mlg."
+  exit 1
+fi
 
 # From where
 remote="rcmp@142.90.96.93"
@@ -12,5 +19,5 @@ dirs=("Raw" "Cluster" "Data" "Filter" "Merger")
 #Act on dirs
 for d in "${dirs[@]}"; do
   echo "Rsyncing ${d}"
-  rsync -e "ssh -i ${SSH_KEY}" -avz "${remote}:/home/rcmp/S2384/RootFiles/${d}/" "./RootFiles/{d}/"
+  rsync -e "ssh -i ${SSH_KEY}" -avz --progress "${remote}:/home/rcmp/S2384/RootFiles/${d}/" "./RootFiles/${d}/"
 done
