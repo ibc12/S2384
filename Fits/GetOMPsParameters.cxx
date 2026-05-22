654cadfc1028ac7ae2d7a172002db875c1ff1af5
ibc12 <ivan.blanco.ordes@gmail.com>
2026-05-22 18:38:59 +0200

    a lot of theo calcs. First ADWA with FRESCO finite range and zero range

--- Fits/GetOMPsParameters.cxx
+++ Fits/GetOMPsParameters.cxx
@@ -5,16 +5,21 @@
 void GetOMPsParameters()
 {
     int Z = 3;               // Lithium
-    int A = 11;               // Lithium-11
+    int A = 8;               // Lithium-11
     double energy {};
-    if(Z == 3 && A == 7)
+    if(Z == 3)
     {
         energy = 7.26557; // 7Li beam time
     }
-    else if(Z == 3 && A == 11)
+    else if(Z == 3)
     {
         energy = 7.37188; // 11Li beam time
     }
+    std::cout << "===================" << std::endl;
+    std::cout << "===================" << std::endl;
+    std::cout << "Getting OMP parameters for Z = " << Z << ", A = " << A << ", energy = " << energy << " MeV" << std::endl;
+    std::cout << "===================" << std::endl;
+    std::cout << "===================" << std::endl;
 
     // Print OMP parameters for different OMPs
     PhysOMP::Haixia haixia(Z, A, energy);
